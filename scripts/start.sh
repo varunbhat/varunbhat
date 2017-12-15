f952def853e94d44c90bcf8af99b4940fdaa6e09
Varun Bhat <varunbhat.kn@gmail.com>
2017-12-15 14:53:14 -0800

    created template

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,13 @@
 
 
 apt-get update
-apt-get install ruby-full build-essential -y
+apt-get install ruby-full build-essential libz-dev curl -y
 
 gem install jekyll bundler
+cd /src
+gem cleanup
+bundle install
+bundle exec jekyll serve --host 0.0.0.0
 
-bundle exec jekyll serve
+
 
